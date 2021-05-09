40892c7ae255c7fe593293d5c31ca4cb448c1646
Dylan Travis <dylan.travis23@outlook.com>
2021-05-09 11:26:18 -0700

    replySchema

---
+++ models/Reply.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const replySchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    boxId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "replies",
+  }
+);
+
+module.exports = mongoose.model("Reply", replySchema);
