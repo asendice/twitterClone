febdc886c7d22e32edebc01662ef0b866bdb980c
Dylan Travis <dylan.travis23@outlook.com>
2021-05-24 13:14:50 -0700

    removed redux devtools

--- server.js
+++ server.js
@@ -22,11 +22,30 @@ mongoose
 //middleware
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
-
-
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(!new Error("Not allowed by CORS "));
+    }
+  },
+};
+app.use(cors(corsOptions));
 app.use("/api", authRoutes);
+
+// need for production
+if (process.env.NODE_ENT === "production") {
+  //serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // handle react routing, return all requests to React App
+}
 
 const port = process.env.PORT;
 
